acfd6e6533844e6ecceada2f32316232d0758442
DDKFM <maxschaedlich@hotmail.de>
2018-03-05 19:47:17 +0100

    import HCloud-API in Monitoring

--- src/main/java/de/ddkfm/hcloud/monitoring/Main.kt
+++ src/main/java/de/ddkfm/hcloud/monitoring/Main.kt
@@ -1,8 +1,12 @@
 package de.ddkfm.hcloud.monitoring
 
+import de.ddkfm.hcloud.HCloudApi
 import spark.kotlin.Http
 import spark.kotlin.ignite
 import spark.ModelAndView
+import spark.Request
+import spark.Response
+import spark.ResponseTransformer
 import spark.template.velocity.VelocityTemplateEngine
 import java.util.HashMap
 
@@ -11,6 +15,7 @@ import java.util.HashMap
 fun main(args : Array<String>) {
     var http : Http = ignite()
     http.port(3000)
+    http.get("/") { response.redirect("/dashboard")}
     http.get("dashboard"){
         var model = mutableMapOf<String, Any?>()
         model.put("test", "HalloWelt")
